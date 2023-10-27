7c28848eb9a337af45c479f848f842325f2f1de9
saaid323.com <saaid2538@.com>
2023-10-27 13:13:50 +0000

    added states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import jsonify
+from flask import jsonify, abort
 from models.base_model import BaseModel
 from api.v1.views import app_views
 from models import storage
@@ -9,4 +9,16 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
     all_items = storage.all(State)
-    return all_items.to_dict()
+    lis = []
+    for i in all_items.values():
+        lis.append(i.to_dict())
+    return jsonify(lis)
+
+
+@app_views.route('/statesi/<state_id>', methods=['GET'], strict_slashes=False)
+def one_state(state_id):
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
