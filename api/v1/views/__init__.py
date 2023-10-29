7a5e14ce5def6ea26cbeeb59ac6cbd95bd34bc90
Theideabased <ogunmusireseyi01@gmail.com>
2023-10-29 14:48:39 -0700

    views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,17 @@
 from flask import Blueprint
+# import the storage engine needed
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+
+# import the views used
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.places import *
+from api.v1.views.amenity import *
